0a340e2e79f742b17f4914293a531748092b8c73
aslam-enkaizen <aslam.hossain@enkaizen.com>
2022-10-05 20:52:24 +0600

    initial

---
+++ src/main/java/com/example/oauth2_client_keycloak/Oauth2ClientKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_client_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ClientKeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ClientKeycloakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oauth2_client_keycloak/Oauth2ClientKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_client_keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ClientKeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
