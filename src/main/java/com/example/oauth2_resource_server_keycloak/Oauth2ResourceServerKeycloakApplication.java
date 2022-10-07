dc013759ef5cff0c010026025049adedee6310cb
aslam-enkaizen <aslam.hossain@enkaizen.com>
2022-10-07 08:18:01 +0600

    Some update

--- src/main/java/com/example/oauth2_resource_server_keycloak/Oauth2ResourceServerKeycloakApplication.java
+++ src/main/java/com/example/oauth2_resource_server_keycloak/Oauth2ResourceServerKeycloakApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Oauth2ClientKeycloakApplication {
+public class Oauth2ResourceServerKeycloakApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Oauth2ClientKeycloakApplication.class, args);
+        SpringApplication.run(Oauth2ResourceServerKeycloakApplication.class, args);
     }
 
 }
